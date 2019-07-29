75bfff63408fdd156df024a52ff28041dab9a737
DUONG HOANG VU <paceyvu@gmail.com>
2019-07-29 20:52:37 +0700

    Add files via upload

---
+++ Module_01/js251_duong/Module_01/Bai_02/Ung_dung_Bien_Co_so/Xu_ly/Xu_ly_3L.js
@@ -0,0 +1,11 @@
+var Duong_dan_Du_lieu=`../Du_lieu_Luu_tru`;
+
+function Doc_Du_lieu_Ho_ten_TXT(){
+    var Tap_tin=`ho_ten.txt`
+    var Dia_chi_Xu_ly=`${Duong_dan_Du_lieu}/${Tap_tin}`
+    var Xu_ly_HTTP=new XMLHttpRequest()
+    Xu_ly_HTTP.open(`GET`,`${Dia_chi_Xu_ly}`,false) 
+    Xu_ly_HTTP.send()
+    var Chuoi_ket_qua=Xu_ly_HTTP.responseText.trim() // Nội dung trả về
+    return Chuoi_ket_qua
+}
